fb39caa5f068c76c65ec7cb35122c26eca185d2c
davidangel <david@davidangel.net>
2017-12-15 09:30:49 -0500

    Modify env_config.sample.py.
    Clarify structure of SURVEY_IDS.

--- analytics/env_config.sample.py
+++ analytics/env_config.sample.py
@@ -2,7 +2,9 @@
 ENV = {
     'SECRET_KEY': 'secret key',
     'QUALTRICS_API_TOKEN': "token",
-    'SURVEY_IDS': [],
+    'SURVEY_IDS': {
+        '': {'site':'', 'survey_type':''}
+    },
     'QUALTRICS_DATA_CENTER_ID': '',
 }
 
